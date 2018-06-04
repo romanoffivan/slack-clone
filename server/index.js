b91ccf937fb773fc8cf991f66afbca5ae98e35f2
romanoffivan <iw.romanof@yandex.ru>
2018-06-04 15:09:07 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import socket from 'socket.io';
 import http from 'http';
 import Router from 'koa-router';
 import koaLogger from 'koa-logger';
-import serve from 'koa-static';
+// import serve from 'koa-static';
 import middleware from 'koa-webpack';
 import bodyParser from 'koa-bodyparser';
 import session from 'koa-generic-session';
@@ -20,13 +20,10 @@ export default () => {
   app.keys = ['some secret hurr'];
   app.use(session(app));
   app.use(bodyParser());
-  if (process.env.NODE_ENV === 'production') {
-    app.use(serve(path.join(__dirname, '..', 'dist', 'assets')));
-  } else {
-    app.use(middleware({
-      config: webpackConfig,
-    }));
-  }
+  // app.use(serve(path.join(__dirname, '..', 'public')));
+  app.use(middleware({
+    config: webpackConfig,
+  }));
 
   const router = new Router();
 
